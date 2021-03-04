d03d87994520771ea3c3a30eaf2849c8b46a1543
JNnikiema <57503027+JNnikiema@users.noreply.github.com>
2021-03-04 19:48:43 +0100

    Update CreationOfLTABTripleStore.java

--- interfaceTerminologyCreation/CreationOfLTABTripleStore.java
+++ interfaceTerminologyCreation/CreationOfLTABTripleStore.java
@@ -11,10 +11,10 @@ public class CreationOfLTABTripleStore {
 
 	public static void main(String[] args) throws RepositoryException, RDFParseException, IOException {
 		// TODO Auto-generated method stub
-		//String namespace = "TLAB";
+		
 		String namespace = "TLABANDLOINC";
-		String cheminFichier ="./TLAB.rdf";
-		//String cheminFichier ="/home/erias/Bureau/TerminologieLocale/synergie.skos" ;
+		//String cheminFichier ="./TLAB.rdf";
+		String cheminFichier ="./TLAB.skos" ;
 		String URIdelaterminologie = "http://erias.org/InterfaceTerminology/TLAB";
 		//ChargementFichierOntologie.ChargementFichier(namespace, cheminFichier, URIdelaterminologie,"text/turtle");
 		ChargementFichierOntologie.ChargementFichier(namespace, cheminFichier, URIdelaterminologie,"application/xml");
